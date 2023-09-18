05619c1f76242219a57c7f7e10265ca7256a55db
bazile <bazile@users.noreply.github.com>
2023-09-18 20:50:08 +0300

    Fix drop tables script for HR sample

--- HR/drop-tables.sql
+++ HR/drop-tables.sql
@@ -1,5 +1,7 @@
-DROP TABLE employees;
+UPDATE employees SET department_id = NULL;
+
 DROP TABLE dependents;
+DROP TABLE employees;
 DROP TABLE departments;
 DROP TABLE locations;
 DROP TABLE countries;
